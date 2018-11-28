89ac7130f93c3aa51f85d9f8131b0f3c01962703
Akhil Narang <akhilnarang.1999@gmail.com>
2018-11-28 18:08:45 +0100

    beryllium: RR!
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- rr.mk
+++ rr.mk
@@ -1,16 +1,16 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018 The rrOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common rr stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := rr_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Poco F1
